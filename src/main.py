8dc645543eaef781c6d1d77f3bbbd88f3c5bea33
caro28 <33645803+caro28@users.noreply.github.com>
2025-03-27 10:07:33 +0100

    Refactor and enhance Open Payments and Prescribers data filtering
    - Renamed and split the `filter_open_payments` function into two distinct functions: `filter_open_payments_levenshtein` for Levenshtein distance matching and `filter_open_payments` for exact drug name matching.
    - Implemented chunked processing for filtering Open Payments and Prescribers data, improving efficiency and memory management.
    - Added logging for matched rows and saved filtered chunks to CSV files.
    - Updated `concatenate_chunks` function to accept a file output parameter for flexibility.
    - Harmonized column names and added new columns in the data cleaning process, ensuring consistency across datasets.

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ def get_ref_drug_names():
     return list(set(ref_drug_names))
 
     
-def filter_open_payments(year, dataset_type, max_distance=1):
+def filter_open_payments_levenshtein(year, dataset_type, max_distance=1):
     """
     Filter Open Payments data for a given year and dataset type
     (General or Research) based on drug name matches with 
@@ -116,49 +116,63 @@ def filter_open_payments(year, dataset_type, max_distance=1):
     logger.info("Matched %s rows for %s %s", matched_rows, year, dataset_type)
 
 
-def concatenate_chunks(chunks_dir, dataset_type, year):
+def filter_open_payments(year, dataset_type):
+    """
+    Filter Open Payments data for a given year and dataset type
+    (General or Research) based on exact drug name matches
+    """
+    # get cleaned drug names (brand and generic) from ProstateDrugList.csv
+    ref_drug_names = get_ref_drug_names()
+
+    # # Load OP data for year/type
+    op_path = get_op_raw_path(year, dataset_type)
+    # load csv in chunks
+    chunksize = 100_000
+    chunks = pd.read_csv(op_path, chunksize=chunksize)
+    
+    # Filter each chunk using exact drug name matches
+    total_matched_rows = 0
+    # Get drug columns
+    op_drug_cols = get_op_drug_columns(pd.read_csv(op_path, nrows=1))
+    dir_out = f"data/filtered/{dataset_type}_payments/chunks/"
+    for i, chunk in enumerate(chunks):
+        matched_rows, row_idx = find_matches(chunk, op_drug_cols, ref_drug_names)
+        logger.info("Matched %s rows", matched_rows)
+        filtered_chunk = chunk.iloc[row_idx]
+        # Save to CSV if filtered chunk is not empty
+        if not filtered_chunk.empty:
+            filtered_chunk.to_csv(f"{dir_out}{dataset_type}_{year}_chunk_{i+1}.csv", index=False)
+            logger.info("Saved chunk %s, found %s matches", i+1, matched_rows)
+            total_matched_rows += matched_rows
+
+    logger.info("Matched %s rows for %s %s", total_matched_rows, year, dataset_type)
+
+
+def concatenate_chunks(chunks_dir, fileout):
     """
     Concatenate all chunks vertically into a single CSV
     """
     # Get all chunk files
     chunks = os.listdir(chunks_dir)
-    output_file = f"data/filtered/{dataset_type}_payments/{dataset_type}_{year}.csv"
     
     # Write first chunk with header
-    pd.read_csv(os.path.join(chunks_dir, chunks[0])).to_csv(output_file, index=False)
+    pd.read_csv(os.path.join(chunks_dir, chunks[0])).to_csv(fileout, index=False)
     
     # Append all other chunks without headers
     for chunk in chunks[1:]:
         pd.read_csv(os.path.join(chunks_dir, chunk)).to_csv(
-            output_file, 
+            fileout, 
             mode='a', 
             header=False, 
             index=False
         )
-    
-    logger.info("Saved %s %s", dataset_type, year)
 
 
-
-# Step 1b: Prescribers Filtering
-# TODO: process in chunks - slow for just 3 years
-def filter_prescribers_by_drug_names(path_in, path_out):
-    """
-    Filter Prescribers data to find qualifying NPIs
-    NB: Early years may be missing NPIs; get those from https://openpaymentsdata.cms.gov/dataset/23160558-6742-54ff-8b9f-cac7d514ff4e
-    """
-    drug_names = ['bicalutamide', 'abiraterone', 'enzalutamide', 'apalutamide', 'darolutamide']
-
-    # Load prescriber data
-    prescribers_filtered_type = pd.read_csv(path_in)
-    
-    # Filter rows with drug names in Brnd_Name or Gnrc_Name
-    drug_cols = ['Brnd_Name', 'Gnrc_Name']
-    
+def find_matches(df, drug_cols, drug_names):
     row_idx = []
     matched_rows = 0
     # iterate through rows
-    for idx, row in prescribers_filtered_type.iterrows():
+    for idx, row in df.iterrows():
         # iterate through drug_cols
         for col in drug_cols:
             drug_name = str(row[col])
@@ -175,11 +189,40 @@ def filter_prescribers_by_drug_names(path_in, path_out):
                     break
                 else:
                     continue
-    
-    logger.info("Matched %s rows", matched_rows)
-    filtered_prescribers = prescribers_filtered_type.iloc[row_idx]
-    #save to csv
-    filtered_prescribers.to_csv(path_out, index=False)
+                break
+
+    return matched_rows, row_idx
+
+
+# Step 1b: Prescribers Filtering
+def filter_prescribers_by_drug_names(path_in, fileout):
+    """
+    Filter Prescribers data to find qualifying NPIs
+    NB: Early years are missing NPIs; get those from https://openpaymentsdata.cms.gov/dataset/23160558-6742-54ff-8b9f-cac7d514ff4e
+    """
+    drug_names = ['bicalutamide', 'abiraterone', 'enzalutamide', 'apalutamide', 'darolutamide']
+
+    # Chunk the df prescribers_filtered_type into 100_000 rows, then filter each chunk
+    chunksize = 100_000
+    chunks = pd.read_csv(path_in, chunksize=chunksize)
+    # Filter rows with drug names in Brnd_Name or Gnrc_Name
+    total_matched_rows = 0
+    drug_cols = ['Brnd_Name', 'Gnrc_Name']
+    dir_out = "data/filtered/prescribers/chunks/"
+    for i, chunk in enumerate(chunks):
+        matched_rows, row_idx = find_matches(chunk, drug_cols, drug_names)
+        logger.info("Matched %s rows", matched_rows)
+        filtered_chunk = chunk.iloc[row_idx]
+        # save filtered chunk to csv if not empty
+        if not filtered_chunk.empty:
+            filtered_chunk.to_csv(f"{dir_out}prescribers_chunk_{i+1}.csv", index=False)
+            logger.info("Saved chunk %s, found %s matches", i+1, matched_rows)
+            total_matched_rows += matched_rows
+
+    logger.info("Matched %s rows for prescribers", total_matched_rows)
+
+    # Concatenate all chunks vertically into a single CSV
+    concatenate_chunks(dir_out, fileout)
 
 
 # Step 2: Group by id and get sorted unique years where target_names appeared
@@ -205,7 +248,6 @@ def get_final_npis(filtered_prescribers):
     final_df = final_df[['Prscrbr_NPI', 'Prscrbr_Type', 'Brnd_Name', 'Gnrc_Name']]
     # save to csv
     final_df.to_csv("data/filtered/prescribers/prescribers_final_npis.csv", index=False)
-    return final_df
 
 
 def build_map_year2cols():
@@ -248,29 +290,37 @@ def build_ref_data_maps():
     return brand2generic, brand2color
 
 
-# Step 2: Data Cleaning & Enhancement
-def clean_op_data(filepath, year2cols, brand2generic, brand2color, npi_set, fileout, year):
+def harmonize_col_names(df, year):
     """
-    Clean and enhance Open Payments data
+    Harmonize column names using a map of year to columns from grace_cols.csv
     """
-    # 1. Harmonize column names
-    df = pd.read_csv(filepath)
-    # drop levenshtein_distance column
-    df.drop(columns='levenshtein_distance', inplace=True)
-    # rename columns
+    # Get map of year2cols
+    year2cols = build_map_year2cols()
     df.columns = year2cols[year]
+    return df
 
-    # 2. Add Prostate_drug_type (0/1 based on Color)
-    # 3. Normalize drug names by adding Drug_Name col with generic names
-    # instantiate drug_cols using range: Drug_Biological_Device_Med_Sup_1 through Drug_Biological_Device_Med_Sup_5
+
+def get_harmonized_drug_cols(df):
     prefix = "Drug_Biological_Device_Med_Sup_"
-    drug_cols = [col for col in df.columns if col.lower().startswith(prefix.lower())]
-    
+    return [col for col in df.columns if col.lower().startswith(prefix.lower())]
+
+
+def get_set_npis():
+    # load npi_list
+    npi_path="data/filtered/prescribers/prescribers_final_npis.csv"
+    npi_df = pd.read_csv(npi_path)
+    npi_set = npi_df['Prscrbr_NPI']
+    assert len(npi_set.value_counts().unique()) == 1
+    return npi_set
+
+# TODO: goal is to only run this for our target drugs, so we should get out of the loop if drug_name is not in brand2generic?
+def add_new_columns(df, drug_cols):
+    brand2generic, brand2color = build_ref_data_maps()
+    npi_set = get_set_npis()
     for idx, row in df.iterrows():
         # iterate through drug_cols
         for col in drug_cols:
             print(f"Processing col {col}")
-            # get drug_name
             drug_name = str(row[col])
             if pd.isna(drug_name) or drug_name == 'nan':
                 continue
@@ -278,15 +328,11 @@ def clean_op_data(filepath, year2cols, brand2generic, brand2color, npi_set, file
                 continue
             drug_name = clean_brand_name(drug_name)
             
-            # TODO: goal is to only run this for our target drugs, so we should get out of the loop if drug_name is not in brand2generic
-            # TODO: is this correct?
+            # add generic_name to df
             try:
-                # get generic_name
                 generic_name = brand2generic[drug_name]
             except KeyError:
                 continue
-            
-            # add generic_name to df
             df.at[idx, 'Drug_Name'] = generic_name
             print("added value to Drug_Name")
 
@@ -297,15 +343,35 @@ def clean_op_data(filepath, year2cols, brand2generic, brand2color, npi_set, file
             # add Onc_Prescriber col: 1 if Prostate_Drug_Type == 1 AND Covered_Recipient_NPI is in npi_set
             df.at[idx, 'Onc_Prescriber'] = 1 if df.at[idx, 'Prostate_Drug_Type'] == 1 and df.at[idx, 'Covered_Recipient_NPI'] in npi_set else 0
             print("added value to Onc_Prescriber")
+    return df
+    
 
-    # save to csv
-    df.to_csv(fileout, index=False)
+# Step 2: Data Cleaning & Enhancement
+def clean_op_data(filepath, npi_set, fileout, year):
+    """
+    Clean and enhance Open Payments data
+    Harmonize column names 
+    Add columns
+        Prostate_drug_type, (0/1 based on Color)
+        Drug_Name, (generic name)
+        Onc_Prescriber, (1 if Prostate_drug_type == 1 AND Covered_Recipient_NPI is in npi_set)
+    """
+    # 1. Harmonize column names
+    df = pd.read_csv(filepath)
+    df = harmonize_col_names(df, year)
+
+    # 2. Add Columns: Drug_Name, Prostate_drug_type, Onc_Prescriber
+    drug_cols = get_harmonized_drug_cols(df)
+    df = add_new_columns(df, drug_cols, npi_set)
+    assert 'Drug_Name' in df.columns
+    assert 'Prostate_drug_type' in df.columns
+    assert 'Onc_Prescriber' in df.columns
+    
+    # 3. Save to CSV (save all cols as string)
+    df.astype(str).to_csv(fileout, index=False)
 
 
 def run_op_cleaner(files_dir, dataset_type):
-    # Get map of year2cols
-    year2cols = build_map_year2cols()
-    brand2generic, brand2color = build_ref_data_maps()
     # load npi_list
     npi_path="data/filtered/prescribers/prescribers_final_npis.csv"
     npi_df = pd.read_csv(npi_path)
@@ -321,10 +387,7 @@ def run_op_cleaner(files_dir, dataset_type):
             fileout = f"data/final_files/{dataset_type}_payments/{dataset_type}_{year}.csv"
             
             clean_op_data(
-                os.path.join(files_dir, file), 
-                year2cols,
-                brand2generic,
-                brand2color,
+                os.path.join(files_dir, file),
                 npi_set,
                 fileout,
                 year
@@ -334,39 +397,46 @@ def run_op_cleaner(files_dir, dataset_type):
 
 
 def main():
-    # # Filter Open Payments (2014-2023)
+    # # 1. Filter Open Payments (2014-2023)
     years = range(2023, 2024) # Use 2023 only for now to draft code
-    for year in years:
-        filter_open_payments(year, "general")
-        print(f"Finished filtering general payments for {year}")
-    #     filter_open_payments(year, "research")
+    dataset_types = ["general"]
+    # Filter in chunks and save intermediary files
+    for dataset_type in dataset_types:
+        for year in years:
+            filter_open_payments(year, dataset_type)
+            print(f"Finished filtering {dataset_type} payments for {year}")
     
-    # Concatenate 2023
-    concatenate_chunks("data/filtered/general_payments/chunks/", "general", 2023)
-    print("Finished concatenating general payments for 2023")
-    # load 2023 filtered OP df
-    final_df = pd.read_csv("data/filtered/general_payments/general_2023.csv")
+    # Concatenate intermediary files
+    for dataset_type in dataset_types:
+        op_chunks_dirs = f"data/filtered/{dataset_type}_payments/chunks/"
+        for year in years:
+            output_file = f"data/filtered/{dataset_type}_payments/chunks/{dataset_type}_{year}.csv"
+            concatenate_chunks(op_chunks_dirs, output_file)
+            print(f"Finished concatenating {dataset_type} payments for {year}")
 
-    # Filter Prescribers
-    path_in = "data/filtered/prescribers/prescribers_filtered_prscrb_type.csv"
-    path_out = "data/filtered/prescribers/prescribers_filtered_type_drug_names.csv"
-    filter_prescribers_by_drug_names(path_in, path_out)
+    # 2. Filter Prescribers
+    # Concatenate raw prescribers chunks (already filtered by prescriber type)
+    prescribers_chunks_dir = "data/filtered/prescribers/chunks/"
+    prescribers_filtered_prscrb_type = "data/filtered/prescribers/prescribers_filtered_prscrb_type.csv"
+    concatenate_chunks(prescribers_chunks_dir, prescribers_filtered_prscrb_type)
+    print("Finished concatenating prescribers chunks")
+    # Filter Prescribers by drug names
+    prescribers_filtered_drug_names = "data/filtered/prescribers/prescribers_filtered_type_drug_names.csv"
+    filter_prescribers_by_drug_names(prescribers_filtered_prscrb_type, prescribers_filtered_drug_names)
     print("Finished filtering prescribers by drug names")
-    # load prescribers filtered by type and drug names
-    path_in = "data/filtered/prescribers/prescribers_filtered_type_drug_names.csv"
-    prescribers_filtered = pd.read_csv(path_in)
     
-    # get final npis
-    final_npis = get_final_npis(prescribers_filtered)
+    # 3. Get target set of NPIs and save to CSV
+    get_final_npis(prescribers_filtered_drug_names)
     print("Finished getting final npis")
     
-    # Clean  all files
+    # 4. Harmonize Open Payments data and Add columns/flags
     for year in years:
         # for dataset_type in ["general", "research"]:
-        for dataset_type in ["general"]:
-            files_dir = f"data/filtered/{dataset_type}_payments_0326/"
-            run_op_cleaner(files_dir, dataset_type)
+        for dataset_type in dataset_types:
+            filtered_op_files_dir = f"data/filtered/{dataset_type}_payments/"
+            run_op_cleaner(filtered_op_files_dir, dataset_type)
             print(f"Finished cleaning {dataset_type} payments for {year}")
+
 
 
 if __name__ == "__main__":
